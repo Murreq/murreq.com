a461d927231cd10b8b5a323a3a6e6388d8c15a9e
Gareth Nel <gareth.w.nel@gmail.com>
2023-11-16 15:23:15 +0200

    made my changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/murreq___",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@murreq___",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:murreq@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "murreq@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Murreq",
 		label: "Github",
-		handle: "chronark",
+		handle: "Murreq",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "murreq.com",
+    template: "%s | murreq.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "murreq.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://murreq.com",
+    siteName: "murrreq.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "Muu.png",
         width: 1920,
         height: 1080,
       },
@@ -38,11 +38,11 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Murreq",
     card: "summary_large_image",
   },
   icons: {
-    shortcut: "/favicon.png",
+    shortcut: "Muu.png",
   },
 };
 const inter = Inter({
